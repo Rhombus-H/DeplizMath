bd1105324a877c6ebc2444fa2ef99807d491bb2d
Rhombus-H <rombworkbruh@gmail.com>
2026-03-13 00:56:53 +0300

    some small additions to tokenizer.cpp

--- src/parser/tokenizer.cpp
+++ src/parser/tokenizer.cpp
@@ -1,4 +1,5 @@
 #include "parser/tokenizer.h"
+#include "core/errors.h"
 #include <cctype>
 
 Tokenizer::Tokenizer(const std::string& input)
@@ -15,6 +16,23 @@ void Tokenizer::skipWhitespace() {
     }
 }
 
+Token Tokenizer::readNumber() {
+    std::size_t start = m_pos;
+
+    // БАГ: не обрабатываем дробную часть правильно
+    while (m_pos < m_input.size() && m_input[m_pos] >= '0' && m_input[m_pos] <= '9') {
+        m_pos++;
+    }
+
+    // пробуем обработать точку (но с багом)
+    if (m_pos < m_input.size() && m_input[m_pos] == '.') {
+        m_pos++;
+    }
+
+    std::string numStr = m_input.substr(start, m_pos - start);
+    return Token(TokenType::Number, numStr, start);
+}
+
 Token Tokenizer::next() {
     skipWhitespace();
 
@@ -23,10 +41,17 @@ Token Tokenizer::next() {
     }
 
     char c = m_input[m_pos];
+
+    if (c >= '0' && c <= '9') {
+        return readNumber();
+    }
+
     m_pos++;
 
     if (c == '+') return Token(TokenType::Plus, "+", m_pos - 1);
     if (c == '-') return Token(TokenType::Minus, "-", m_pos - 1);
+    if (c == '*') return Token(TokenType::Star, "*", m_pos - 1);
+    if (c == '/') return Token(TokenType::Slash, "/", m_pos - 1);
 
     return Token(TokenType::Unknown, std::string(1, c), m_pos - 1);
 }
