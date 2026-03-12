78179bf986ff6bdb699ad6a5cdd9162d1fc32149
Rhombus-H <rombworkbruh@gmail.com>
2026-03-13 02:00:34 +0300

    Add parsing equality-formatted input

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -19,3 +19,38 @@ void Parser::expect(TokenType type) {
     advance();
 }
 
+// ParseResult Parser::parse() {
+//     // парсим левую часть
+//     ExprPtr leftSide = parseExpr();
+//
+//     // обязательно должен быть оператор: = > < >= <=
+//     if (current_token.type == TokenType::EndOfInput) {
+//         throw ParseError("Expected '=' or comparison operator. Use format: y = f(x) or lhs = rhs");
+//     }
+//
+//     // === ЗНАК РАВНО -> функция или уравнение ===
+//     if (current_token.type == TokenType::Equal) {
+//         advance();
+//         ExprPtr rightSide = parseExpr();
+//
+//         if (current_token.type != TokenType::EndOfInput) {
+//             throw UnexpectedTokenError(current_token.value, current_token.position);
+//         }
+//
+//         // если lhs это просто "y" -> пробуем как функцию
+//         // (pipeline потом доп. проверит содержит ли rhs y)
+//         VariableLit* varCheck = std::get_if<VariableLit>(&leftSide->data);
+//         if (varCheck != nullptr && varCheck->name == "y") {
+//             // пока считаем что это функция. если rhs содержит y,
+//             // pipeline переклассифицирует в уравнение
+//             ParsedFunction func;
+//             func.expr = std::move(rightSide);
+//             return func;
+//         }
+//
+//         // иначе уравнение (x^2+y^2=1, x=y^2, и тд)
+//         ParsedEquation eq;
+//         eq.lhs = std::move(leftSide);
+//         eq.rhs = std::move(rightSide);
+//         return eq;
+//     }
