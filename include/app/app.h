3edd14e780538f1be9091cba12a23434fb48cb74
anna-potter <anyanpotter@gmail.com>
2026-03-13 11:30:15 +0300

    Add another header in app dir

---
+++ include/app/app.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <array>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "renderer/viewport.h"
+#include "renderer/plotter.h"
+#include "renderer/grid_renderer.h"
+#include "app/pipeline.h"
+#include "app/history.h"
+
+class App {
+public:
+    App();
+    ~App();
+
+    void run(); // основной цикл
+
+private:
+    // shared_ptr потому что viewport используется в нескольких местах
+    std::shared_ptr<Viewport> viewport;
+    std::unique_ptr<Plotter> plotter;
+    std::unique_ptr<GridRenderer> gridRenderer;
+    Pipeline pipeline;
+    History history;
+
+    sf::RenderWindow window;
+    bool isRunning = true;
+
+    // ui stuff
+    std::array<char, 256> inputBuf;
+    std::string statusMsg;
+    float vpInputs[4]; // xmin, xmax, ymin, ymax
+
+    void handleEvents();
+    void updateViewport();
+    void drawUI();
+    void drawControlPanel();
+    void drawPlotWindow();
+    void saveSnapshot();
+};
